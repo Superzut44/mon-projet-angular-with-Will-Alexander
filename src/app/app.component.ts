29636a35bf86b79ef1fac668a73fd4d30a45035d
chavaudrey <chavaudreyxav@hotmail.fr>
2022-05-27 16:17:01 +0200

    Pipes date and uppercase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isAuth = false;
 
+  lastUpdate = new Date();
+
   appareils = [
     {
       name: 'Machine à laver',
-      status: 'éteint'
+      status: 'allumé'
     },
     {
       name: 'Télévision',
