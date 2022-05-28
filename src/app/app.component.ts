8c27aadc12ba63aa8cf3a65bf43408d50e93280e
chavaudrey <chavaudreyxav@hotmail.fr>
2022-05-28 13:52:03 +0200

    integrated routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,11 @@
-import { DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  isAuth = false;
-
-  lastUpdate: Promise<Date> = new Promise(
-    (resolve, reject) => {
-      const date = new Date();
-      setTimeout(
-        () => {
-          resolve(date);
-        }, 2000
-      );
-    }
-  );
-
-  appareils!: any[];
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-
-  ngOnInit(): void {
-      this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    this.appareilService.switchOffAll();
-  }
+export class AppComponent {
+  constructor() {}
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,27 @@ import localeFr from '@angular/common/locales/fr';
 registerLocaleData(localeFr, 'fr');
 
 import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
     AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AppareilService
